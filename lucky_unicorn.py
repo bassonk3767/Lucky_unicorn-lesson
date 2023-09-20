c9852a3c66f75a02109683a669dfedf066530d05
bassonk3767 <145512278+bassonk3767@users.noreply.github.com>
2023-09-21 11:00:32 +1200

    Add files via upload

--- lucky_unicorn.py
+++ lucky_unicorn.py
@@ -1,3 +1,5 @@
+import random as rnd
+
 # rounds
 i = 0
 
@@ -10,9 +12,24 @@ def play(a,b):
     if a >= 5 and point == 4:
         print('\n\nYOU WIN!!\n')
     else:
-        print('\n\nPlease insert the amount that you wish to bet:\n\n')
-        bet = int(input())
-        
+
+        # Asking user if they know how to play.
+
+        print('\n\nDo you know how to play?\n\n[1]> Yes , [2]> No\n\n')
+        knowledge = int(input())
+
+        if knowledge == 1:
+            print('\n\nPlease insert the amount that you wish to bet:\n\n')
+            bet = int(input())
+
+        # generating dealer's value
+
+            dealers = rnd.randint( bet - 2 , bet + 2)
+
+            print("\n\n")
+
+        elif knowledge == 2:
+             
 
 while i < 10:
     print('\n\nPlease deposit $1 to play LUCKY UNICORN!!\n\n[1]> Yes , [2]> No\n\n')
