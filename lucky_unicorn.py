ae9ca5a2de1647452eb3e2e2643dab8680a4dbe5
bassonk3767 <145512278+bassonk3767@users.noreply.github.com>
2023-09-21 10:47:10 +1200

    Add files via upload

--- lucky_unicorn.py
+++ lucky_unicorn.py
@@ -1,1 +1,28 @@
-def
+# rounds
+i = 0
+
+# points
+point = 0
+
+
+def play(a,b):
+    bb =  'ff'
+    if a >= 5 and point == 4:
+        print('\n\nYOU WIN!!\n')
+    else:
+        print('\n\nPlease insert the amount that you wish to bet:\n\n')
+        bet = int(input())
+        
+
+while i < 10:
+    print('\n\nPlease deposit $1 to play LUCKY UNICORN!!\n\n[1]> Yes , [2]> No\n\n')
+    ii = int(input())
+    if ii == 1:
+        i += 1
+        play()
+    elif ii == 2:
+          print('\n\nDeactivating game\n\n')
+          break
+    else:
+        print(''
+              '\n\nPlease insert your choice that was mentioned on screen')
